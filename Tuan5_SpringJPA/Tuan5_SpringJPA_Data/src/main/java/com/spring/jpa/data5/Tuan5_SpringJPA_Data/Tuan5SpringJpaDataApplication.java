09595c3ecb3fa928b261042d563f2faa4db5e771
nguyenthanhnhan <nhannhan01052001@gmail.com>
2022-09-27 01:53:35 +0700

    add week 5

---
+++ Tuan5_SpringJPA/Tuan5_SpringJPA_Data/src/main/java/com/spring/jpa/data5/Tuan5_SpringJPA_Data/Tuan5SpringJpaDataApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.data5.Tuan5_SpringJPA_Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuan5SpringJpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuan5SpringJpaDataApplication.class, args);
+	}
+
+}
---
+++ Tuan5_SpringJPA/Tuan5_SpringJPA_Data/src/test/java/com/spring/jpa/data5/Tuan5_SpringJPA_Data/Tuan5SpringJpaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.data5.Tuan5_SpringJPA_Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuan5SpringJpaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
